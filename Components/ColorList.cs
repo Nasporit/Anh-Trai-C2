a85482060d692ddbbd9b83e8da3b68f7e567496c
Nasporit <160713625+Nasporit@users.noreply.github.com>
2024-09-02 21:55:30 +0700

    Add files via upload

---
+++ Components/ColorList.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SneakersShop.Areas.Identity.Data;
+
+namespace SneakersShop.Components
+{
+	public class ColorList : ViewComponent
+	{
+		private readonly ApplicationDbContext _context;
+		public ColorList(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+		public IViewComponentResult Invoke()
+		{
+			return View("Index", _context.Colors.ToList());
+		}
+	}
+}
---
+++ Components/GenderList.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SneakersShop.Areas.Identity.Data;
+
+namespace SneakersShop.Components
+{
+	public class GenderList : ViewComponent
+	{
+		private readonly ApplicationDbContext _context;
+		public GenderList(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+		public IViewComponentResult Invoke()
+		{
+			return View("Index", _context.Genders.ToList());
+		}
+	}
+}
