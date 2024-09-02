442e7b1773ac98dc05ffe06ed17ab636f5e1e865
Nasporit <160713625+Nasporit@users.noreply.github.com>
2024-09-02 22:15:38 +0700

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SneakersShop.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
