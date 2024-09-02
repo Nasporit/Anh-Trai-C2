609a1286de7e2aa44ad1f62627e8ef1f5479ffbc
Nasporit <160713625+Nasporit@users.noreply.github.com>
2024-09-02 21:55:06 +0700

    Add files via upload

---
+++ Areas/Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SneakersShop.Areas.Identity.Data;
+using SneakersShop.Models;
+
+namespace SneakersShop.Areas.Identity.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Brand> Brands { get; set; }
+    public DbSet<Color> Colors { get; set; }
+    public DbSet<Gender> Genders { get; set; }
+    public DbSet<Size> Sizes { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
